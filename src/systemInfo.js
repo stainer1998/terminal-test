8a64ba7b90f519e7c18bcda84406940edbc9181a
Javier Vallejo <javistainer98@gmail.com>
2022-04-18 20:07:17 -0400

    add users, bios, baseboard, chassis, graphics, networkConnections, fsSize, shell, audio

--- src/systemInfo.js
+++ src/systemInfo.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const os = require("os");
 const si = require("systeminformation");
 const { formatBytes } = require("./helper/formatBytes.js")
@@ -18,10 +17,47 @@ try {
     console.error(e);
 }
 
-si.cpuTemperature()
+/* si.cpuTemperature()
     .then(data => console.log(`CPU Temperature: ${data.main} °C`))
     .catch(error => console.error(error));
 
+si.users()
+    .then(data => console.log('user:' + data[0].user))
+    .catch(error => console.error(error));
+
+si.bios()
+    .then(data => console.log('bios: ' + data.version))
+    .catch(error => console.error(error));
+
+si.baseboard()
+    .then(data => console.log('baseboard: ' + data.version))
+    .catch(error => console.error(error));
+
+si.chassis()
+    .then(data => console.log('chasis: '+ data.version))
+    .catch(error => console.error(error))
+
+si.graphics()
+    .then(data => console.log(data.displays))
+    .catch(error => console.error(error))
+
+si.networkConnections()
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+si.fsSize()
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+si.shell()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.audio()
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+    
+ */
 function printResults(title, disks) {
 
     console.log(`============ ${title} ==============\n`);
